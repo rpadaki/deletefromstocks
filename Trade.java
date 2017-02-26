7d3c3af50898c2f880f8a21cbfddcd92d357a847
Jacob Klegar <jklegar@Jacobs-MacBook-Air-4.local>
2017-02-26 11:42:14 -0500

    Added files

---
+++ Trade.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Trade {
+	public static void tradebond(int id, boolean buy) {
+		// always have orders of 99-101 in
+		
+		return
+	}
+
+	public static void tradeval() {
+		/* if one is higher than the other, buy the first and sell the second
+		issues comes up if one is always higher than the other
+		at that point buy if one is more than 10 higher than the other and convert
+		*/ 
+	}
+
+	pubic static void tradexlf() {
+		/* 
+		
+		*/
+	}
+}
