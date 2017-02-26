da7f954f1a750dcc7289647bc3e2f8cb1789a90e
Alexander Wei <cheddarcheeseyumz@yahoo.com>
2017-02-26 17:59:06 -0500

    asdf

--- Stock.java
+++ Stock.java
@@ -1,58 +1,52 @@
 import java.util.*;
 
 public class Stock {
-  String symbol;
-  ArrayList<Position> marketBids, marketOffers;
-  ArrayList<Order> ourBids, ourOffers;
-  int limit, portfolio;
+    String symbol;
+    ArrayList<Position> marketBids, marketOffers;
+    ArrayList<Order> ourBids, ourOffers;
+    int limit, portfolio;
 
-  public Stock(String symbol, int limit) {
-    this.symbol = symbol;
-    this.limit = limit;
-    ourBids = new ArrayList<>();
-    ourOffers = new ArrayList<>();
-    marketBids = new ArrayList<>();
-    marketOffers = new ArrayList<>();
-  }
-
-  public void updatePositions(ArrayList<Position> bids, ArrayList<Position> offers) {
-    this.marketBids = bids;
-    this.marketOffers = offers;
-  }
-
-  public Position bestBid() {
-    if(marketBids.size() > 0) {
-      return marketBids.get(0);
-    } else {
-      return null;
+    public Stock(String symbol, int limit) {
+        this.symbol = symbol;
+        this.limit = limit;
+        ourBids = new ArrayList<>();
+        ourOffers = new ArrayList<>();
+        marketBids = new ArrayList<>();
+        marketOffers = new ArrayList<>();
     }
-  }
 
-  public Position bestOffer() {
-    if(marketOffers.size() > 0) {
-      return marketOffers.get(0);
-    } else {
-      return null;
+    public void updatePositions(ArrayList<Position> bids, ArrayList<Position> offers) {
+        assert(bids.size() > 0);
+        assert(offers.size() > 0);
+        this.marketBids = bids;
+        this.marketOffers = offers;
     }
-  }
 
-  public int selling() {
-      int tot = 0;
-      for(Order ord : ourOffers) {
-          tot += ord.pos.size;
-      }
-      return tot;
-  }
-
-  public int buying() {
-    int tot = 0;
-    for(Order ord : ourBids) {
-      tot += ord.pos.size;
+    public Position bestBid() {
+        return marketBids.get(0);
     }
-    return tot;
-  }
 
-  public int q() {
-    return portfolio;
-  }
+    public Position bestOffer() {
+        return marketOffers.get(0);
+    }
+
+    public int selling() {
+        int tot = 0;
+        for(Order ord : ourOffers) {
+            tot += ord.pos.size;
+        }
+        return tot;
+    }
+
+    public int buying() {
+        int tot = 0;
+        for(Order ord : ourBids) {
+            tot += ord.pos.size;
+        }
+        return tot;
+    }
+
+    public int q() {
+        return portfolio;
+    }
 }
